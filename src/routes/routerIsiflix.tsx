a814f7422ca088ae3d89b47d78f15a8367be1ace
Yanis RAJBAOUI <yanis.rajbaoui@acedigitalecom.onmicrosoft.com>
2022-03-28 13:33:12 +0200

    Configuring prettier & eslint

--- src/routes/routerIsiflix.tsx
+++ src/routes/routerIsiflix.tsx
@@ -1,15 +1,14 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ROOT_ROUTE } from './routes';
 
-
 export default function RouterIsiflix() {
-    return (
-        <Router>
-        <Routes>
-        <Route path={ROOT_ROUTE}  />
+  return (
+    <Router>
+      <Routes>
+        <Route path={ROOT_ROUTE} />
         {/* <Route path="*" /> */}
-        </Routes>
-
-        </Router>
-    );
+      </Routes>
+    </Router>
+  );
 }
