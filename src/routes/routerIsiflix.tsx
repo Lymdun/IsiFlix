01f2efb625d4c2a9869df0f281f8bc8e42e037d6
Yanis RAJBAOUI <yanis.rajbaoui@acedigitalecom.onmicrosoft.com>
2022-03-28 14:01:13 +0200

    Arborescence de dossier

--- src/routes/routerIsiflix.tsx
+++ src/routes/routerIsiflix.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from '../pages/Authentication/Login/Login';
 import { ROOT_ROUTE } from './routes';
 
 export default function RouterIsiflix() {
   return (
     <Router>
       <Routes>
-        <Route path={ROOT_ROUTE} />
+        <Route path={ROOT_ROUTE} element={<Login />} />
         {/* <Route path="*" /> */}
       </Routes>
     </Router>
