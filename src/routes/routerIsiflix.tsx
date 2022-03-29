b8aa14662293434a5f6361f6bb4e0c5a7df4bdb6
Lymdun <lymdun@protonmail.com>
2022-03-29 12:02:07 +0200

    feat: ajout page 404

--- src/routes/routerIsiflix.tsx
+++ src/routes/routerIsiflix.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Browse from '../pages/Browse/Browse';
-import { ROOT_ROUTE } from './routes';
+import Error from '../pages/Error/Error';
+import { ROOT_ROUTE, NO_MATCH_ROUTE } from './routes';
 
 export default function RouterIsiflix() {
   return (
     <Router>
       <Routes>
         <Route path={ROOT_ROUTE} element={<Browse />} />
-        {/* <Route path="*" /> */}
+        <Route path={NO_MATCH_ROUTE} element={<Error />} />
       </Routes>
     </Router>
   );
