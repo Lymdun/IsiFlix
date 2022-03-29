379cc78afb738b63de6c20d940d6b225e2766cb4
Yanis RAJBAOUI <yanis.rajbaoui@acedigitalecom.onmicrosoft.com>
2022-03-29 11:31:18 +0200

    Browse page WIP

---
+++ src/models/MovieItem.ts
@@ -0,0 +1,5 @@
+export interface MovieItem {
+  id?: string;
+  poster_path?: string;
+  original_title?: string;
+}
--- src/routes/routerIsiflix.tsx
+++ src/routes/routerIsiflix.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from '../pages/Authentication/Login/Login';
+import Browse from '../pages/Browse/Browse';
 import { ROOT_ROUTE } from './routes';
 
 export default function RouterIsiflix() {
   return (
     <Router>
       <Routes>
-        <Route path={ROOT_ROUTE} element={<Login />} />
+        <Route path={ROOT_ROUTE} element={<Browse />} />
         {/* <Route path="*" /> */}
       </Routes>
     </Router>
