cc4eb83db245192f278f3f5f70103af38e5b922b
Yanis RAJBAOUI <yanis.rajbaoui@acedigitalecom.onmicrosoft.com>
2022-03-29 14:55:59 +0200

    MyStore

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from './components/header/header';
 import RouterIsiflix from './routes/routerIsiflix';
+import { ContextProvider } from './store/Store';
 
 function App() {
   return (
     <div>
-      <Header />
-      <RouterIsiflix />
+      <ContextProvider>
+        <Header />
+        <RouterIsiflix />
+      </ContextProvider>
     </div>
   );
 }
